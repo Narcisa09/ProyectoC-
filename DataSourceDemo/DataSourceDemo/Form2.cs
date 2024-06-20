b482d9611e8854be10f85f6687d12e8adb99b2ba
Narcisa09 <144978618+Narcisa09@users.noreply.github.com>
2024-06-19 23:18:34 -0500

    Add files via upload

---
+++ DataSourceDemo/DataSourceDemo/Form2.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DataSourceDemo
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void customersBindingNavigatorSaveItem_Click(object sender, EventArgs e)
+        {
+            this.Validate();
+            this.customersBindingSource.EndEdit();
+            this.tableAdapterManager.UpdateAll(this.northwindDataSet);
+
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'northwindDataSet.Customers' table. You can move, or remove it, as needed.
+            this.customersTableAdapter.Fill(this.northwindDataSet.Customers);
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void contactNameTextBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void customersBindingSource_CurrentChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            customersBindingSource.AddNew();
+        }
+
+
+        private void toolStripTextBox1_KeyPress_1(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == (char)13)
+            {
+                var index = customersBindingSource.Find("CustomerID", toolStripTextBox1.Text);
+                if (index != -1) {
+                    customersBindingSource.Position = index;
+                    return;
+                }
+                MessageBox.Show("ID no encontrada");
+            }
+            
+        }
+    }
+}
